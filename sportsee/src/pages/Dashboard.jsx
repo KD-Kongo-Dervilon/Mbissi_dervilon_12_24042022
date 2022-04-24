59b918bbf705b99b07f6a8eb055bf4cd1db125af
Tydis KD <82412416+KD-Kongo-Dervilon@users.noreply.github.com>
2022-04-24 23:00:59 +0200

    setting up the scss

--- sportsee/src/pages/Dashboard.jsx
+++ sportsee/src/pages/Dashboard.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import Header from '../components/dashboard/Header';
+import Poids from '../components/dashboard/Poids';
+import Objectifs from '../components/dashboard/Objectifs';
+import Radar from '../components/dashboard/Radar';
+import Score from '../components/dashboard/Score';
+import Counter from '../components/dashboard/Counter';
+
 
 const Dashboard = () => {
     return (
-        <div className="Counter">
-            
+        <div className="Dashboard">
+            <Header />
+            <div className="dashboard_graphs">
+                <div className="graphs_left">
+                    <Poids/>
+                    <Objectifs />
+                    <Radar />
+                    <Score/>
+                    <Counter />
+                </div>
+                <div className="graphs_right">
+
+                </div>
+            </div>
         </div>
     );
 };
